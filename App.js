33c44763d3ac336193f0ea4e8730205402e48edb
Matthzao <mateusmoraes100@gmail.com>
2022-09-08 17:08:29 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [newSkill, setNewSkil] = useState();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <StatusBar style="light" />
+      <Text style={estilos.title}>Welcome, Matthew</Text>
+      <TextInput onChangeText={setNewSkil} placeholderTextColor='#555' placeholder='New Skill' style={estilos.input} />
+      <TouchableOpacity style={estilos.button}>
+        <Text style={estilos.buttonText}>ADD</Text>
+      </TouchableOpacity>
+      <Text style={[estilos.title, { color: 'white' }]}>
+        {newSkill}
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#121015',
+    paddingVertical: 80
+
   },
+  title: {
+    color: 'white',
+    marginLeft: 25,
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: 'red',
+    padding: 14,
+    marginTop: 30,
+    borderRadius: 6,
+    fontSize: 18,
+    width: 350,
+    marginLeft: 11
+  },
+  button: {
+    backgroundColor: 'purple',
+    width: 50,
+    alignItems: 'center',
+    borderRadius: 6,
+    marginLeft: 310,
+    padding: 5,
+    marginVertical: 15,
+    fontWeight: 'bold'
+
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  // skills: {
+  //   color: 'white',
+  //   marginLeft: 25,
+  //   fontSize: 25,
+  //   fontWeight: 'bold'
+  // }
+
 });
+
